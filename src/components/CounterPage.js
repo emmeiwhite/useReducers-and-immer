3e53cb92563904cb134bf6bfebed8f074d90eca9
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-12-23 12:13:15 +0530

    We just used switch instead of if-else but it is just a matter of choice

--- src/components/CounterPage.js
+++ src/components/CounterPage.js
@@ -1,26 +1,93 @@
-import React, { useState } from "react";
-import FormLot from "./FormLot";
+import React, { useReducer } from "react";
+// import FormLot from "./FormLot";
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const LOT = "LOT";
+const LOT_SUMUP = "LOT_SUMUP";
+
+const defaultState = {
+  count: 10,
+  lot: 0,
+};
+
+const reducer = (state, action) => {
+  /*
+  if (action.type === INCREMENT) {
+    return { ...state, count: state.count + 1 };
+  }
+
+  if (action.type === DECREMENT) {
+    return { ...state, count: state.count - 1 };
+  }
+  if (action.type === LOT) {
+    return { ...state, lot: action.payload };
+  }
+
+  if (action.type === LOT_SUMUP) {
+    return { ...state, count: state.count + state.lot };
+  }
+   return state;
+  */
+
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + 1 };
+
+    case DECREMENT:
+      return { ...state, count: state.count - 1 };
+
+    case LOT:
+      console.log("LOT");
+      return { ...state, lot: action.payload };
+
+    case LOT_SUMUP:
+      return { ...state, count: state.count + state.lot };
+
+    default:
+      return state;
+  }
+};
 
 export default function CounterPage() {
-  const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
-  const addMore = (lotValue) => {
-    setCount(count + lotValue);
+  const handleChange = (e) => {
+    let valueToAdd = parseInt(e.target.value) || 0; //To avoid NaN with empty string
+
+    console.log("OnChange");
+    dispatch({
+      type: LOT,
+      payload: valueToAdd || "",
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!state.lot) return;
+    // addMore(parseFloat(state.lot));
+
+    dispatch({
+      type: LOT_SUMUP,
+    });
   };
 
   return (
     <div>
-      <h1>Current Count: {count}</h1>
+      <h1>Current Count: {state.count}</h1>
 
       <button
         className="btn"
-        onClick={() => setCount((prev) => prev + 1)}
+        // onClick={() => setCount((prev) => prev + 1)}
+        onClick={() => dispatch({ type: "INCREMENT" })}
       >
         Increment
       </button>
       <button
         className="btn"
-        onClick={() => setCount((prev) => prev - 1)}
+        // onClick={() => setCount((prev) => prev - 1)}
+        onClick={() => dispatch({ type: "DECREMENT" })}
       >
         Decrement
       </button>
@@ -28,11 +95,11 @@ export default function CounterPage() {
       {/* <h3>Add a lot</h3> */}
 
       {/* Keeping form here means whenever we type anything in the form controlled field, our whole app will re-render */}
-      {/* <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="number"
-          value={lot}
-          onChange={(e) => setLot(e.target.value)}
+          value={state.lot}
+          onChange={(e) => handleChange(e)}
         />
         <button
           type="submit"
@@ -40,10 +107,12 @@ export default function CounterPage() {
         >
           Add it
         </button>
-      </form> */}
+      </form>
 
       {/* Lowering the state for less re-renders */}
-      <FormLot addMore={addMore} />
+      {/* <FormLot
+        addMore={addMore}
+      /> */}
     </div>
   );
 }
